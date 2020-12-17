362acf0f71fbd54648b8be62c1f7b31c14a4b93c
Alexander Eschenbrenner <alexander.eschenbrenner@plentymarkets.com>
2020-12-17 13:16:06 +0100

    Hello World Backend Init

---
+++ src/Providers/HelloWorldServiceProvider.php
@@ -0,0 +1,13 @@
+<?php
+namespace HelloWorld\Providers;
+
+use Plenty\Plugin\ServiceProvider;
+
+
+class HelloWorldServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+
+    }
+}
